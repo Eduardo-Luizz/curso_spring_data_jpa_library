addc8dd0785192ef7c4acf75d62ca3ac83d8672d
Eduardo Luiz <eduardoluizzanoto@gmail.com>
2025-01-02 09:03:50 -0300

    Fix - corrigindo o tipo do ID

--- src/main/java/io/github/eduardoluiz/libraryapi/model/Livro.java
+++ src/main/java/io/github/eduardoluiz/libraryapi/model/Livro.java
@@ -3,7 +3,9 @@ package io.github.eduardoluiz.libraryapi.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Table(name = "livro", schema = "public")
@@ -13,7 +15,7 @@ public class Livro {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.UUID)
-    private int id;
+    private UUID id;
 
     @Column(name = "isbn", length = 20, nullable = false)
     private String isbn;
@@ -29,7 +31,7 @@ public class Livro {
     private GeneroLivro genero;
 
     @Column(name = "preco", precision = 18, scale = 2)
-    private Double preco;
+    private BigDecimal preco;
 
     @JoinColumn(name = "id_autor")
     @ManyToOne
