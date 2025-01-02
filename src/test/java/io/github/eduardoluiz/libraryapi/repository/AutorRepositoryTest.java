4e7199ae47812c277ed31b7253d08c354e062aba
Eduardo Luiz <eduardoluizzanoto@gmail.com>
2025-01-02 18:02:56 -0300

    Adicionando testes para entidades simples

--- src/test/java/io/github/eduardoluiz/libraryapi/repository/AutorRepositoryTest.java
+++ src/test/java/io/github/eduardoluiz/libraryapi/repository/AutorRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -38,5 +39,30 @@ public class AutorRepositoryTest {
 
             System.out.println(autorEncontrado);
         }
+    }
+
+    @Test
+    public void listarTest() {
+        List<Autor> autores = autorRepository.findAll();
+        autores.forEach(System.out::println);
+    }
+
+    @Test
+    public void countTest() {
+        System.out.println("Total de Autores: " + autorRepository.count());
+    }
+
+    @Test
+    public void deletarPorIdTest() {
+        var id = UUID.fromString("de3243be-c6a2-4d6b-926b-daa01addc4fc");
+        autorRepository.deleteById(id);
+    }
+
+    @Test
+    public void deleteTest() {
+        var id = UUID.fromString("e10c5592-05ec-4ec6-9553-a0e1009ffd4c");
+        var joao = autorRepository.findById(id).get();
+
+        autorRepository.delete(joao);
     }
 }
