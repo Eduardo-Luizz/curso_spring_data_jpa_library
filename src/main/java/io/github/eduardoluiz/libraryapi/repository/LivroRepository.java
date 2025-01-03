629de98f3f2c0a1ba458df4d1bfabd4301a5988d
Eduardo Luiz <eduardoluizzanoto@gmail.com>
2025-01-03 15:24:37 -0300

    Aula que explica sobre a documentação do JPA query methods

--- src/main/java/io/github/eduardoluiz/libraryapi/repository/LivroRepository.java
+++ src/main/java/io/github/eduardoluiz/libraryapi/repository/LivroRepository.java
@@ -5,6 +5,7 @@ import io.github.eduardoluiz.libraryapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -25,4 +26,8 @@ public interface LivroRepository extends JpaRepository<Livro, UUID> {
 
     // select * from livro where titulo = ? and isbn = ?
     List<Livro> findByTituloAndIsbn(String titulo, String isbn);
+
+    // select * from livro where daa_publicacao between ? and ?
+    List<Livro> findByDataPublicacaoBetween(LocalDate inicio, LocalDate fim);
+
 }
