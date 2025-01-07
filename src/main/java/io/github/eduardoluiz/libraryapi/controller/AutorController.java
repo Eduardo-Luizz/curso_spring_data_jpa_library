c79a7ad5ea6be507600044b5df65b967f9eb161f
Eduardo Luiz <eduardoluizzanoto@gmail.com>
2025-01-07 17:00:25 -0300

    feat(AutorController): Adicionar endpoint para criação de autores

--- src/main/java/io/github/eduardoluiz/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/eduardoluiz/libraryapi/controller/AutorController.java
@@ -1,9 +1,40 @@
 package io.github.eduardoluiz.libraryapi.controller;
 
+import io.github.eduardoluiz.libraryapi.controller.dto.AutorDTO;
+import io.github.eduardoluiz.libraryapi.model.Autor;
+import io.github.eduardoluiz.libraryapi.service.AutorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/autores")
 public class AutorController {
+
+    private final AutorService autorService;
+
+    public AutorController(AutorService autorService) {
+        this.autorService = autorService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> salvarAutor(@RequestBody AutorDTO autor) {
+
+        Autor autorEntidade = autor.mapearParaAutor();
+        autorService.salvar(autorEntidade);
+
+        // http://localhost:8080/autores/UUID
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(autorEntidade.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
 }
