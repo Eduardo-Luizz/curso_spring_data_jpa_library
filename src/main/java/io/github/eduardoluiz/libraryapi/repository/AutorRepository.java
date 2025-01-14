655cbd55f7fed6a2b7b77939985a895c536ba8a6
Eduardo Luiz <eduardoluizzanoto@gmail.com>
2025-01-13 21:53:33 -0300

    feat - implementando a pesquisa de autores com filtro

--- src/main/java/io/github/eduardoluiz/libraryapi/repository/AutorRepository.java
+++ src/main/java/io/github/eduardoluiz/libraryapi/repository/AutorRepository.java
@@ -3,8 +3,12 @@ package io.github.eduardoluiz.libraryapi.repository;
 import io.github.eduardoluiz.libraryapi.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AutorRepository extends JpaRepository<Autor, UUID> {
 
+    List<Autor> findByNome(String nome);
+    List<Autor> findByNacionalidade(String nacionalidade);
+    List<Autor> findByNomeAndNacionalidade(String nome, String nacionalidade);
 }
