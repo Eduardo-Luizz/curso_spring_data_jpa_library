8621532240f5b7897a5e1de15530d61ff059f15d
Eduardo Luiz <eduardoluizzanoto@gmail.com>
2025-01-30 20:27:48 -0300

    feat - Criando UserDetailService customizado para buscar autores do banco

--- src/main/java/io/github/eduardoluiz/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/eduardoluiz/libraryapi/config/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package io.github.eduardoluiz.libraryapi.config;
 
+import io.github.eduardoluiz.libraryapi.security.CustomUserDetailService;
+import io.github.eduardoluiz.libraryapi.service.UsuarioService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,12 +9,9 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -44,20 +43,8 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+    public UserDetailsService userDetailsService(UsuarioService usuarioService) {
 
-        UserDetails user1 = User.builder()
-                .username("Raul")
-                .password(encoder.encode("3451"))
-                .roles("USER")
-                .build();
-
-        UserDetails user2 = User.builder()
-                .username("admin")
-                .password(encoder.encode("5656"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user1, user2);
+        return new CustomUserDetailService(usuarioService);
     }
 }
---
+++ src/main/java/io/github/eduardoluiz/libraryapi/security/CustomUserDetailService.java
@@ -0,0 +1,30 @@
+package io.github.eduardoluiz.libraryapi.security;
+
+import io.github.eduardoluiz.libraryapi.model.Usuario;
+import io.github.eduardoluiz.libraryapi.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UsuarioService usuarioService;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        Usuario usuario = usuarioService.obterPorLogin(login);
+
+        if (usuario == null) {
+            throw new UsernameNotFoundException("Usuário não encontrado");
+        }
+
+        return User.builder()
+                .username(usuario.getLogin())
+                .password(usuario.getSenha())
+                .roles(usuario.getRoles().toArray(new String[usuario.getRoles().size()]))
+                .build();
+    }
+}
